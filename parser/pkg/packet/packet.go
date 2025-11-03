f18a1b7b46ea25f22b7ae50c86c374a62ea1dae9
reindeer002 <lsj000424@gmail.com>
2025-11-03 13:13:05 +0000

    feat: add packet frame structure

---
+++ parser/pkg/packet/packet.go
@@ -0,0 +1,21 @@
+package packet
+
+type MonadPacketChunk struct {
+	// 헤더 (Signature 이후)
+	Signature       [65]byte // Signature of sender
+	Version         uint16   // 2 bytes
+	Flags           byte     // 1 byte (broadcast, secondary_broadcast, unused)
+	MerkleTreeDepth byte     // 4 bits from Flags byte
+	Epoch           uint64   // 8 bytes (u64)
+	TimestampMs     uint64   // 8 bytes (u64)
+	AppMessageHash  [20]byte // 20 bytes (first 20 bytes of hash of AppMessage)
+	AppMessageLen   uint32   // 4 bytes (u32)
+	MerkleProof     [][]byte // 20 bytes * (MerkleTreeDepth - 1)
+
+	// 청크 특정 정보
+	FirstHopRecipient [20]byte // 20 bytes (first 20 bytes of hash of chunk's first hop recipient)
+	MerkleLeafIdx     byte     // 1 byte
+	Reserved          byte     // 1 byte
+	ChunkID           uint16   // 2 bytes (u16)
+	Payload           []byte   // rest of data
+}
